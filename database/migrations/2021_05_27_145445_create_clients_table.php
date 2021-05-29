858fcde13acd36208be4d49ca08b33b9ca599162
MeftunH <maftunhashimli@gmail.com>
2021-05-29 06:45:20 +0400

    heroku

---
+++ database/migrations/2021_05_27_145445_create_clients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->text('image');
+            $table->boolean('status')->default(1);
+            $table->boolean('is_slide_content')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
