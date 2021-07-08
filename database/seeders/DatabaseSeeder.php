068b25594906b3c2c643763da86edc18f9fea506
MeftunH <maftunhashimli@gmail.com>
2021-07-08 18:16:08 +0400

    seeder update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,15 +15,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        DB::table('users')->insert([
-            'name' => 'User1',
-            'email' => 'user1@email.com',
-            'password' => bcrypt('password'),
+
+        DB::table('languages')->insert([
+            'name' => 'English',
+            'code' => 'en-US',
+            'flag' => '/storage/public/images/flag_img/60a7b11981ddd.jpg',
+            'status' => '1',
+            'is_default' => '1',
         ]);
-        DB::table('users')->insert([
-            'name' => 'user2',
-            'email' => 'user2@email.com',
-            'password' => bcrypt('password'),
-        ]);
+//        DB::table('users')->insert([
+//            'name' => 'user2',
+//            'email' => 'user2@email.com',
+//            'password' => bcrypt('password'),
+//        ]);
     }
 }
